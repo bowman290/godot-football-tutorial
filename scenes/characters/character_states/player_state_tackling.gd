56c94b78968dba081c8a3d0ab09e060924588bc7
bowman290 <bowman290@gmail.com>
2025-08-31 09:25:07 +0100

    refactor: add friction to tackle

--- scenes/characters/character_states/player_state_tackling.gd
+++ scenes/characters/character_states/player_state_tackling.gd
@@ -2,6 +2,7 @@ class_name PlayerStateTackling
 extends PlayerState
 
 const DURATION_TACKLE := 200
+const GROUND_FRICTION := 250.0
 
 var time_start_tackle := Time.get_ticks_msec()
 
@@ -9,7 +10,11 @@ func _enter_tree() -> void:
 	animation_player.play("tackle")
 	time_start_tackle = Time.get_ticks_msec()
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
+	player.velocity = player.velocity.move_toward(Vector2.ZERO, delta * GROUND_FRICTION)
 	if Time.get_ticks_msec() - time_start_tackle > DURATION_TACKLE:
-		state_transition_requested.emit(Player.State.RECOVERING)
-	
+		start_recovery()
+		
+func start_recovery():
+	await get_tree().create_timer(0.2).timeout
+	state_transition_requested.emit(Player.State.RECOVERING)
